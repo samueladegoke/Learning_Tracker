caf454400926b61cc6a891c4b5deeee19cd7b351
samueladegoke <samade1996@gmail.com>
2025-12-09 19:13:07 +0100

    Fix Netlify deployment: Include backend function, requirements, config

--- netlify/functions/api/__init__.py
+++ netlify/functions/api/__init__.py
@@ -5,7 +5,7 @@ from mangum import Mangum
 # Add the project root to sys.path so we can import backend
 # Assumes this file is in netlify/functions/api.py
 current_dir = os.path.dirname(os.path.abspath(__file__))
-root_path = os.path.abspath(os.path.join(current_dir, '../../'))
+root_path = os.path.abspath(os.path.join(current_dir, '../../../'))
 
 # Add both root and backend directory to path
 sys.path.insert(0, root_path)
